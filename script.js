04fa624b06e0c0caa939d831806ef1918083021b
MaloGns <malo.guinoiseau@gmail.com>
2026-04-27 13:39:00 +0200

    navbar

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
+function copier(){
+    console.log("Le plagiat est une forme de vol intellectuel qui consiste à copier le travail d'autrui sans autorisation ni attribution appropriée. Il est important de respecter les droits d'auteur et de citer correctement les sources pour éviter le plagiat.");
+}
 function main(){
-
+    document.body.addEventListener("oncopy", copier());
 }
 main();
