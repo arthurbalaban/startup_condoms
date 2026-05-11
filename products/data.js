659a2199f697502256de7ec079da3634f3131756
Arthur Balaban <arthur@balaban.fr>
2026-05-11 10:14:13 +0200

    reinvented products descriptions and funny stuff

--- products/data.js
+++ products/data.js
@@ -1,38 +1,50 @@
 const DATA = [
   {
     "image": "images/square_template.png",
-    "title": "Produit Protection",
-    "description": "Protection maximale avec confort",
+    "title": "Je ne suis pas ton père",
+    "description": "Notre best-seller, pour que la force soit avec vous. 10 préservatifs.",
     "price": "12.99"
   },
   {
     "image": "images/square_template.png",
-    "title": "Confort Plus",
-    "description": "Sensation naturelle améliorée",
-    "price": "14.99"
+    "title": "Envidken",
+    "description": "Pour ne pas s'imposer de limites",
+    "price": "12.99"
   },
   {
     "image": "images/square_template.png",
-    "title": "Protection Premium",
-    "description": "Qualité supérieure garantie",
-    "price": "16.99"
+    "title": "La belle et la bite",
+    "description": "Comme un conte de fées, boite de 10",
+    "price": "11.99"
   },
   {
     "image": "images/square_template.png",
-    "title": "Ultra Fin",
-    "description": "Sensation optimale",
-    "price": "13.99"
+    "title": "T'habites à Durcy ?",
+    "description": "Pour un confort même à domicile, boite de 10",
+    "price": "11.99"
   },
   {
     "image": "images/square_template.png",
-    "title": "Confort Étendu",
-    "description": "Durée prolongée",
-    "price": "15.99"
+    "title": "Harry pelotter et la braguette magique + Try potter",
+    "description": "Pour une soirée magique, combo de 10 de chaque",
+    "price": "21.99"
   },
   {
     "image": "images/square_template.png",
-    "title": "Protection Totale",
-    "description": "Protection renforcée",
-    "price": "17.99"
+    "title": "Tu me donnes la gaule",
+    "description": "Pour les nostalgiques de Vercingétorix, boite de 10",
+    "price": "12.99"
+  },
+  {
+    "image": "images/square_template.png",
+    "title": "Mc hapote + Mc & Ket",
+    "description": "Toute taille disponible, même happy meal. Boite de 10 de chaque.",
+    "price": "19.99"
+  },
+  {
+    "image": "images/square_template.png",
+    "title": "Apéricul",
+    "description": "C'est encore mieux partagé. 10 préservatifs.",
+    "price": "9.99"
   }
 ]
