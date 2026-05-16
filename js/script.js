284dc22d3b5a66673f2e4ebd8af939150c506d13
MaloGns <malo.guinoiseau@gmail.com>
2026-05-16 13:20:19 +0200

    m

--- js/script.js
+++ js/script.js
@@ -24,8 +24,41 @@ function hamburgerMenu(){
     }
 }
 
+function changecolor(){
+    //ne change pas la couleur du texte sur la page d'accueil
+    const page =document.getElementsByTagName("title")[0].innerText;
+    if(page=="CondomStart'"){
+        return;
+    }
+    //définir la couleur
+    const colors=["#FFC6EC", "#EA921F", "#3357FF", "#FF00FF", "#33FFF5"];
+    const colorname=["Rose clair", "Orange", "Bleu", "Magenta", "Cyan"];
+    let colorindex=Math.floor(Math.random()*colors.length);
+
+    //selectionner l'élément
+    // let elt=[];
+    const navbar_elts=document.getElementsByClassName("navlinks")[0].children;
+    for(let i=0; i<navbar_elts.length; i++){
+        if(navbar_elts[i].innerText==page){
+            let elt=navbar_elts[i];
+            console.log(elt);
+            break;
+        }
+    }
+    // elt=elt[0];
+
+    //affichage
+    console.log(elt);
+    // elt.setAttribute("style", "color: " + colors[colorindex]+ ";");
+    elt.style.color=colors[colorindex];
+    console.log(elt);
+    console.log("La couleur du menu a été changée en "+colorname[colorindex]);
+}
+
+
 function main(){
     document.body.addEventListener("copy", copier);
     hamburgerMenu();
+    changecolor();
 }
 main();
